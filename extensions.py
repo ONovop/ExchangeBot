0067b83a970b937864ec0a8cffb81786b5ba8932
Oleg <o.novop@gmail.com>
2022-11-06 13:57:00 +0600

    ispr

--- extensions.py
+++ extensions.py
@@ -8,15 +8,36 @@ class APIException(Exception):
 class Converter():
     @staticmethod
     def get_price(quote, base, amount):
-        page = 'https://www.cbr-xml-daily.ru/daily_json.js'
+        page = f'https://api.exchangerate.host/convert?from={quote}&to={base}&amount={amount}'
         r = requests.get(page)
         dic = json.loads(r.content)
-        if quote != 'RUB':
-            rubles = amount * dic['Valute'][quote]['Value'] / dic['Valute'][quote]['Nominal']
-        else:
-            rubles = amount
-        if base != 'RUB':
-            total = round((rubles * dic['Valute'][base]['Nominal'] / dic['Valute'][base]['Value']), 2)
-        else:
-            total = round(rubles, 2)
+        reply = dic['result']
+        total = round(reply, 2)
         return total
+
+    @staticmethod
+    def convert(message):
+        try:
+            divided = message.text.split(' ')
+            if len(divided) != 3:
+                raise APIException('Неверное количество параметров')
+            quote, base, amount = divided
+            quote = quote.upper()
+            base = base.upper()
+            if quote not in currencies.keys():
+                raise APIException(f'Введена недоступная валюта {quote}')
+            if base not in currencies.keys():
+                raise APIException(f'Введена недоступная валюта {base}')
+            if quote == base:
+                raise APIException(f'Невозможно конвертировать одинаковые валюты {base}')
+            try:
+                amount = float(amount)
+            except ValueError:
+                raise APIException('Количество переводимой валюты должно быть числом')
+        except APIException as e:
+            return f'Неверный формат команды.\n{e}'
+        except Exception as e:
+            return f'Не удалось обработать команду.\n{e}'
+        else:
+            total = Converter.get_price(quote, base, amount)
+            return f'{amount} {currencies[quote]} соответствует {total} {currencies[base]}'
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import telebot
 
-from extensions import APIException, Converter
+from extensions import Converter
 from tok import TOKEN, currencies
 
 bot = telebot.TeleBot(TOKEN)
@@ -21,29 +21,7 @@ def handle_values(message):
 
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
-    try:
-        divided = message.text.split(' ')
-        if len(divided) != 3:
-            raise APIException('Неверное количество параметров')
-        quote, base, amount = divided
-        quote = quote.upper()
-        base = base.upper()
-        if quote not in currencies.keys():
-            raise APIException(f'Введена недоступная валюта {quote}')
-        if base not in currencies.keys():
-            raise APIException(f'Введена недоступная валюта {base}')
-        if quote == base:
-            raise APIException(f'Невозможно конвертировать одинаковые валюты {base}')
-        try:
-            amount = float(amount)
-        except ValueError:
-            raise APIException('Количество переводимой валюты должно быть числом')
-    except APIException as e:
-        bot.reply_to(message, f'Неверный формат команды.\n{e}')
-    except Exception as e:
-        bot.reply_to(message, f'Не удалось обработать команду.\n{e}')
-    else:
-        total = Converter.get_price(quote, base, amount)
-        bot.reply_to(message, f'{amount} {currencies[quote]} соответствует {total} {currencies[base]}')
+    text = Converter.convert(message)
+    bot.reply_to(message, text)
 
 bot.polling(none_stop=True)
